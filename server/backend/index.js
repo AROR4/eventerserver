f4ef6deb61b717346066300d3413e33817a99d72
MehulDhingra <dhingramehul24@gmail.com>
2024-04-29 11:11:41 +0530

    wqe

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,36 +1,9 @@
-require('dotenv').config();
-require('express-async-errors');
-
-const express = require('express');
-// const serverless = require('serverless-http')
+const express = require("express");
 const app = express();
-const mainRouter = require('./routes/main');
-const notFoundMiddleware = require('./middleware/not-found');
-const errorHandlerMiddleware = require('./middleware/error-handler');
-const connectMiddle = require('./middleware/connect');
+const mainApp = require("./app");
 
-// middleware
-app.use(express.static('./public'));
-app.use(express.json());
-app.use(connectMiddle);
-app.use('/api/v1', mainRouter);
-app.use(notFoundMiddleware);
-app.use(errorHandlerMiddleware);
+app.use("/", mainApp);
 
-
-const port = process.env.PORT || 5000;
-
-const start = async () => {
-  try {
-    app.listen(port, () =>
-      console.log(`Server is listening on port ${port}...`)
-    );
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-
-start();
+app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
