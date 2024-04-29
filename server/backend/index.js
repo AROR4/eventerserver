a040295022663301b9d98cdf9879288c829389ac
MehulDhingra <dhingramehul24@gmail.com>
2024-04-29 11:05:33 +0530

    21

--- server/backend/index.js
+++ server/backend/index.js
@@ -1,8 +1,36 @@
-const express = require("express");
+require('dotenv').config();
+require('express-async-errors');
+
+const express = require('express');
+// const serverless = require('serverless-http')
 const app = express();
+const mainRouter = require('./routes/main');
+const notFoundMiddleware = require('./middleware/not-found');
+const errorHandlerMiddleware = require('./middleware/error-handler');
+const connectMiddle = require('./middleware/connect');
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+// middleware
+app.use(express.static('./public'));
+app.use(express.json());
+app.use(connectMiddle);
+app.use('/api/v1', mainRouter);
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+
+const port = process.env.PORT || 5000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+
+start();
 
 module.exports = app;
