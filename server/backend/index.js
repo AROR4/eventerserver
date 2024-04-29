bc31400b9cce47f5fe68104c4aec0a4c51e9cca3
MehulDhingra <dhingramehul24@gmail.com>
2024-04-29 13:03:09 +0530

    21

--- server/backend/index.js
+++ server/backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mainApp = require("./app");
 
-app.use("/", mainApp);
+// app.use("/", mainApp);
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 
